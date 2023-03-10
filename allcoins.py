6d72ae39689a835e44f4690d18b6f50404517677
Hugopvigo <Hugopvigo@gmail.com>
2023-03-10 17:16:27 +0100

    Update Coins & date

--- allcoins.py
+++ allcoins.py
@@ -1,18 +1,24 @@
 import requests
 import schedule
-import datetime as dt
 import time
+import datetime
 from bs4 import BeautifulSoup
 
+# Obtener fecha y hora actual
+ahora = datetime.datetime.now()
+# Formatear la fecha y la hora como una cadena
+cadena = ahora.strftime("%d/%m/%Y %H:%M:%S")
+print(cadena)  
 # Crear una lista de diccionarios con las urls, los nombres y los precios máximos de los productos que quieres analizar
 products = [
-{"url": "https://www.andorrano-joyeria.com/tienda/monedas-de-plata/otros-paises/germania-2023-1oz-plata-info", "name": "Germania", "max_price": 34},
-{"url": "https://www.andorrano-joyeria.com/tienda/monedas-de-plata/estados-unidos/estados-unidos-american-eagle-2023-1oz-plata-info", "name": "American Eagle", "max_price": 34},
-{"url": "https://www.andorrano-joyeria.com/tienda/monedas-de-plata/china/china-panda-2023-30-00g-plata-info", "name": "Panda", "max_price": 32},
-{"url": "https://www.andorrano-joyeria.com/tienda/monedas-de-plata/australia/australia-wedge-tailed-2023-1oz-plata-info", "name": "Wedge Tailed", "max_price": 31},
-{"url": "https://www.andorrano-joyeria.com/tienda/monedas-de-plata/australia/australia-koala-2023-1oz-plata-info", "name": "Koala", "max_price": 29},
+{"url": "https://www.andorrano-joyeria.com/tienda/monedas-de-plata/otros-paises/germania-2023-1oz-plata-info", "name": "Germania", "max_price": 37},
+{"url": "https://www.andorrano-joyeria.com/tienda/monedas-de-plata/estados-unidos/estados-unidos-american-eagle-2023-1oz-plata-info", "name": "American Eagle", "max_price": 33},
+{"url": "https://www.andorrano-joyeria.com/tienda/monedas-de-plata/china/china-panda-2023-30-00g-plata-info", "name": "Panda", "max_price": 31},
+{"url": "https://www.andorrano-joyeria.com/tienda/monedas-de-plata/australia/australia-wedge-tailed-2023-1oz-plata-info", "name": "Wedge Tailed", "max_price": 30},
+{"url": "https://www.andorrano-joyeria.com/tienda/monedas-de-plata/australia/australia-koala-2023-1oz-plata-info", "name": "Koala", "max_price": 28},
 {"url": "https://www.andorrano-joyeria.com/tienda/monedas-de-plata/otros-paises/sudafrica-krugerrand-2023-1oz-plata-info", "name": "Krugerrand", "max_price":  26},
-{"url": "https://www.andorrano-joyeria.com/tienda/monedas-de-plata/reino-unido/reino-unido-britannia-2023-1oz-plata-info", "name": "Britannia", "max_price": 25}
+{"url": "https://www.andorrano-joyeria.com/tienda/monedas-de-plata/reino-unido/reino-unido-britannia-2023-1oz-plata-info", "name": "Britannia", "max_price": 25},
+{"url": "https://www.andorrano-joyeria.com/tienda/monedas-de-plata/austria/austria-filarmonica-2023-1oz-plata-info", "name": "Filarmonica", "max_price": 24}
 ]
 
 # Definir una función que reciba un diccionario como argumento y extraiga el precio de la url usando beautifulsoup
@@ -23,27 +29,25 @@ def check_price(product):
 
   # Crear un objeto BeautifulSoup con el contenido HTML de la respuesta
   soup = BeautifulSoup(response.content, "html.parser")
-  # Determinar hora
-  # dt = time.asctime()
-
+     
   # Buscar el elemento que contiene el precio del producto usando un selector CSS
   price_element = soup.find(class_="Price")
 
   # Extraer el texto del elemento y convertirlo a un número
   price_text = price_element.get_text()
   price_number = float(price_text.replace("€", "").replace(",", "."))
+
 # Comparar el precio extraído con el precio máximo del diccionario y si es menor, imprimir un mensaje con el nombre y el precio del producto
   if price_number < product["max_price"]:
-    print(f"El {product['name']} está a {price_number} euros. ¡Es una buena oferta!")
-    #+ str(dt)
+    print(f"El {product['name']} está a {price_number} euros. ¡Es una buena oferta!")    
   elif price_number > product["max_price"]:
     print(f"{product['name']} está caro")  
 # Usar un bucle for para iterar sobre la lista de diccionarios y llamar a la función para cada uno
 for product in products:
-  check_price(product)
+  check_price(product)    
 # Programar la función para que se ejecute cada dos horas
   schedule.every().hour.do(check_price,product)
-# Ejecutar un bucle infinito que comprueba si hay tareas pendientes
+ # Ejecutar un bucle infinito que comprueba si hay tareas pendientes
 while True:
   schedule.run_pending()
   time.sleep(1)
