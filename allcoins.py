22116d8d163e029d0733c061c0bdd598c88848eb
Hugopvigo <Hugopvigo@gmail.com>
2023-03-14 12:49:17 +0100

    Added xlsx log

--- allcoins.py
+++ allcoins.py
@@ -1,15 +1,30 @@
+#!/usr/bin/python
 import requests
 import schedule
 import time
 import datetime
+import openpyxl
+import os.path
 from bs4 import BeautifulSoup
 from telegram_notifier import TelegramNotifier
-  
-# Obtener fecha y hora actual
-ahora = datetime.datetime.now()
-# Formatear la fecha y la hora como una cadena
-cadena = ahora.strftime("%d/%m/%Y %H:%M:%S")
-print(cadena)  
+
+
+# Verificar si el archivo ya existe   
+filename = f'precioplata.xlsx'
+if os.path.isfile(filename):
+    workbook = openpyxl.load_workbook(filename)
+    sheet = workbook.active
+else:
+  # Crear un libro de Excel
+  workbook = openpyxl.Workbook()
+  # Seleccionar la hoja activa
+  sheet = workbook.active
+  # Escribir encabezados de las columnas
+  sheet['A1'] = 'Fecha y hora'
+  sheet['B1'] = 'Moneda'
+  sheet['C1'] = 'Precio'
+  sheet['D1'] = 'Stock'
+
 # Crear una lista de diccionarios con las urls, los nombres y los precios máximos de los productos que quieres analizar
 products = [
 {"url": "https://www.andorrano-joyeria.com/tienda/monedas-de-plata/otros-paises/germania-2023-1oz-plata-info", "name": "Germania", "max_price": 37},
@@ -38,29 +53,41 @@ def check_price(product):
   # Extraer el texto del elemento y convertirlo a un número
   price_text = price_element.get_text()
   price_number = float(price_text.replace("€", "").replace(",", "."))
+
   # Extraer el texto de stock
   stock_text = stock_element.get_text()
-  stock_status = stock_text.replace("Debido a la situación actual, ciertos productos pueden sufrir retrasos excepcionales.", " ").replace("\n", "")
-  
-# Comparar el precio extraído con el precio máximo del diccionario y si es menor, imprimir un mensaje con el nombre y el precio del producto
-  if price_number < product["max_price"]:
-# Configurar el chat_id y el token del bot
+  stock_status = stock_text.replace("Debido a la situación actual, ciertos productos pueden sufrir retrasos excepcionales.", " ").replace("\n", "").replace("Estado:", "")
+
+  # Obtener fecha y hora actual
+  ahora = datetime.datetime.now()
+  # Formatear la fecha y la hora como una cadena
+  cadena = ahora.strftime("%d/%m/%Y %H:%M") 
+
+  # Comparar el precio extraído con el precio máximo de la lista y si es menor, imprimir un mensaje con el nombre y el precio del producto
+  if price_number < product["max_price"]:     
+    # Configurar el chat_id y el token del bot
     f = open("token.txt", "r")
     Token = f.read()
     chat_id = "1333872" # Tu chat_id
     token = Token # Tu token del bot
     notifier = TelegramNotifier(token=token, chat_id=chat_id, parse_mode="HTML")
-# Enviar un mensaje al chat_id cuando se cumpla una condición    
-    print(f"El {product['name']} está a {price_number} euros. ¡Es una buena oferta! | {stock_status}")
+    # Enviar un mensaje al chat_id cuando se cumpla una condición    
+    print(cadena, f"El {product['name']} está a {price_number} euros. ¡Es una buena oferta! | {stock_status}")
+    row = (cadena, f"{product['name']}", f"{price_number} ", f"{stock_status}")
+    sheet.append(row)
     notifier.send(f"El {product['name']} está a <b>{price_number} euros.</b> | {stock_status} | {cadena} | Compralo en: {product['url']}")
   elif price_number > product["max_price"]:
-    print(f"{product['name']} está caro. {stock_status}")      
-# Usar un bucle for para iterar sobre la lista de diccionarios y llamar a la función para cada uno
+    print(cadena, f"{product['name']} está a {price_number} euros y es caro. {stock_status}")      
+    row = (cadena, f"{product['name']}", f"{price_number}", f"{stock_status}")
+    sheet.append(row)
+    # Guardar el archivo de Excel
+    workbook.save('precioplata.xlsx')
+# Usar un bucle for para iterar sobre la lista de productos y llamar a la función para cada uno
 for product in products:
   check_price(product)    
-# Programar la función para que se ejecute cada dos horas
+  # Programar la función para que se ejecute cada dos horas
   schedule.every().hour.do(check_price,product)
- # Ejecutar un bucle infinito que comprueba si hay tareas pendientes
+# Ejecutar un bucle infinito que comprueba si hay tareas pendientes
 while True:
   schedule.run_pending()
-  time.sleep(1)
+  time.sleep(1)  
